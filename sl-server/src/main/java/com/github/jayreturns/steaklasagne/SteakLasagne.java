4ec838cd91b7320596eb105673908be7502d768e
JayReturns <jay3105@hotmail.com>
2022-04-08 16:20:42 +0200

    [#5] Added Server

---
+++ sl-server/src/main/java/com/github/jayreturns/steaklasagne/SteakLasagne.java
@@ -0,0 +1,9 @@
+package com.github.jayreturns.steaklasagne;
+
+public class SteakLasagne {
+
+    public static void main(String[] args) {
+        System.out.println("Hello SteakLasagne!");
+    }
+
+}
