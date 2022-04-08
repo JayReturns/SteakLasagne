4ad57378084527ebea68eb08deff17e60d628f52
JayReturns <jay3105@hotmail.com>
2022-04-09 00:05:37 +0200

    [#7] Initialized Spring Boot

---
+++ sl-server/src/main/java/com/github/jayreturns/slserver/SlServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.jayreturns.slserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlServerApplication.class, args);
+    }
+
+}
