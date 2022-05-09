8bdda7b925ced1915f1219f93051faef1ea0c581
JayReturns <jay3105@hotmail.com>
2022-05-09 15:30:47 +0200

    [#103] Removed Date Constraint for Transaction

--- sl-server/src/main/java/com/github/jayreturns/slserver/transaction/api/TransactionData.java
+++ sl-server/src/main/java/com/github/jayreturns/slserver/transaction/api/TransactionData.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDateTime;
 
 /**
@@ -25,7 +24,6 @@ public class TransactionData {
     @NotNull
     private Float value;
 
-    @PastOrPresent(message = "Date hast to be in past or present")
     private LocalDateTime date;
 
 }
