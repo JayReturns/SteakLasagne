e0f2fe87c550691b982e8d5b9b58b64951c7aa10
Rexphel <philiprexroth@gmail.com>
2022-05-17 13:46:28 +0200

    Groundwork for Eventlistener

---
+++ sl-server/src/main/java/com/github/jayreturns/slserver/keycloak/events/CustomEventListenerProvider.java
@@ -0,0 +1,106 @@
+package com.github.jayreturns.slserver.keycloak.events;
+
+import org.keycloak.events.Event;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventType;
+import org.keycloak.events.admin.AdminEvent;
+import org.keycloak.events.admin.OperationType;
+
+import java.util.Map;
+import java.util.Set;
+
+public class CustomEventListenerProvider implements EventListenerProvider {
+
+    private Set<EventType> excludedEvents;
+    private Set<OperationType> excludedAdminOperations;
+
+    public CustomEventListenerProvider(Set<EventType> excludedEvents, Set<OperationType> excludedAdminOpearations) {
+        this.excludedEvents = excludedEvents;
+        this.excludedAdminOperations = excludedAdminOpearations;
+    }
+
+    @Override
+    public void onEvent(Event event) {
+        // Ignore excluded events
+        if (excludedEvents != null && excludedEvents.contains(event.getType())) {
+            return;
+        } else {
+            System.out.println("EVENT: " + toString(event));
+        }
+    }
+
+    @Override
+    public void onEvent(AdminEvent event, boolean includeRepresentation) {
+        // Ignore excluded operations
+        if (excludedAdminOperations != null && excludedAdminOperations.contains(event.getOperationType())) {
+            return;
+        } else {
+            System.out.println("EVENT: " + toString(event));
+        }
+    }
+
+    private String toString(Event event) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("type=");
+        sb.append(event.getType());
+        sb.append(", realmId=");
+        sb.append(event.getRealmId());
+        sb.append(", clientId=");
+        sb.append(event.getClientId());
+        sb.append(", userId=");
+        sb.append(event.getUserId());
+        sb.append(", ipAddress=");
+        sb.append(event.getIpAddress());
+
+        if (event.getError() != null) {
+            sb.append(", error=");
+            sb.append(event.getError());
+        }
+
+        if (event.getDetails() != null) {
+            for (Map.Entry<String, String> e : event.getDetails().entrySet()) {
+                sb.append(", ");
+                sb.append(e.getKey());
+                if (e.getValue() == null || e.getValue().indexOf(' ') == -1) {
+                    sb.append("=");
+                    sb.append(e.getValue());
+                } else {
+                    sb.append("='");
+                    sb.append(e.getValue());
+                    sb.append("'");
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private String toString(AdminEvent adminEvent) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("operationType=");
+        sb.append(adminEvent.getOperationType());
+        sb.append(", realmId=");
+        sb.append(adminEvent.getAuthDetails().getRealmId());
+        sb.append(", clientId=");
+        sb.append(adminEvent.getAuthDetails().getClientId());
+        sb.append(", userId=");
+        sb.append(adminEvent.getAuthDetails().getUserId());
+        sb.append(", ipAddress=");
+        sb.append(adminEvent.getAuthDetails().getIpAddress());
+        sb.append(", resourcePath=");
+        sb.append(adminEvent.getResourcePath());
+
+        if (adminEvent.getError() != null) {
+            sb.append(", error=");
+            sb.append(adminEvent.getError());
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public void close() {
+    }
+    }
---
+++ sl-server/src/main/java/com/github/jayreturns/slserver/keycloak/events/CustomEventListenerProviderFactory.java
@@ -0,0 +1,55 @@
+package com.github.jayreturns.slserver.keycloak.events;
+
+import org.keycloak.Config;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventListenerProviderFactory;
+import org.keycloak.events.EventType;
+import org.keycloak.events.admin.OperationType;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CustomEventListenerProviderFactory implements EventListenerProviderFactory{
+
+    private Set<EventType> excludedEvents;
+    private Set<OperationType> excludedAdminOperations;
+
+    @Override
+    public EventListenerProvider create(KeycloakSession session) {
+        return new CustomEventListenerProvider(excludedEvents, excludedAdminOperations);
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+        String[] excludes = config.getArray("exclude-events");
+        if (excludes != null) {
+            excludedEvents = new HashSet<>();
+            for (String e : excludes) {
+                excludedEvents.add(EventType.valueOf(e));
+            }
+        }
+
+        String[] excludesOperations = config.getArray("excludesOperations");
+        if (excludesOperations != null) {
+            excludedAdminOperations = new HashSet<>();
+            for (String e : excludesOperations) {
+                excludedAdminOperations.add(OperationType.valueOf(e));
+            }
+        }
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+
+    }
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public String getId() {
+        return "sysout";
+    }
+}
