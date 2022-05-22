629f6aca89e08745aff3ecd74d04aced41564ba4
JayReturns <jay3105@hotmail.com>
2022-05-22 23:50:54 +0200

    Update SlServerApplication.java

--- sl-server/src/main/java/com/github/jayreturns/slserver/SlServerApplication.java
+++ sl-server/src/main/java/com/github/jayreturns/slserver/SlServerApplication.java
@@ -3,9 +3,6 @@ package com.github.jayreturns.slserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.core.io.ClassPathResource;
-
-import java.io.IOException;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -13,11 +10,6 @@ public class SlServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SlServerApplication.class, args);
-        try {
-            System.out.println(new ClassPathResource("").getFile().getAbsolutePath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
 }
